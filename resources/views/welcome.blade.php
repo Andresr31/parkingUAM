7b62a0b8ff8eae3d5297bcc90b0c6373390d605a
Carlos Andrés Rojas Parra <carlos.andres.1031@gmail.com>
2020-03-28 20:56:28 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<div class="card text-white bg-dark container md-8">
+  <div class="card-header text-center">Welcome</div>
+  <div class="card-body">
+    <h3 class="card-title text-center">PARKING UAM</h3>
+
+    <p class="text-center">Sistema de información se encuentra en desarrollo - Ingenieria de software II - UAM</p>
+    
+  </div>
+</div>
+
+@endsection
